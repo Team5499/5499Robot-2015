f426a99e931070dbcdaa762afe17c7c11898d0ef
Irina Lavryonova <lavryonovairina@gmail.com>
2015-01-17 16:12:49 -0800

    Updated existing subsystems to CAN

--- src/org/usfirst/frc/team5499/robot/RobotMap.java
+++ src/org/usfirst/frc/team5499/robot/RobotMap.java
@@ -11,17 +11,22 @@ public class RobotMap {
     // public static int leftMotor = 1;
     // public static int rightMotor = 2;
     
-	public static int gripperMotor1Channel = 0;
-	public static int gripperMotor2Channel = 0;
-	public static int lifterMotorChannel = 0;
 	public static int frontLeftWheelnum = 0;
 	public static int frontRightWheelnum = 0;
 	public static int backLeftWheelnum = 0;
 	public static int backRightWheelnum = 0;
+	
+	/*
+	 * ID nums for motors (CAN)
+	 */
 	public static int motorFrontLeftid = 1;
 	public static int motorFrontRightid = 2;
 	public static int motorBackLeftid = 3;
 	public static int motorBackRightid = 4;
+	public static int gripperMotor1id = 5;
+	public static int gripperMotor2id = 6;
+	public static int lifterMotor1id = 7;
+	public static int lifterMotor2id = 8;
 	
     // If you are using multiple modules, make sure to define both the port
     // number and the module. For example you with a rangefinder:
